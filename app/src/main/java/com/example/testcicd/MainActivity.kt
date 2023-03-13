4368e17843c1a6d9bb8d8517b5e2291a6948ed60
Ibtehaaj Shahid <ibtehaaj.shahid@netsoltech.com>
2023-03-13 14:04:50 +0500

    added detekt configuration

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.testcicd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    var HELLO by Delegates.notNull<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
