7ff0d84c6012b6f7b47648251104c3fd01d67534
Ibtehaaj Shahid <ibtehaaj.shahid@netsoltech.com>
2023-07-13 10:17:57 +0500

    check todo rule

--- app/src/main/java/com/example/testcicd/MainActivity.kt
+++ app/src/main/java/com/example/testcicd/MainActivity.kt
@@ -9,9 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //todo:nhufsbaufbashufbuashfbu
     }
 
-    fun un() {
-        val a =2
-    }
 }
