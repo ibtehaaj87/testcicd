73dffdaab074b7986d3052be748f2f8fc27bc705
Ibtehaaj Shahid <ibtehaaj.shahid@netsoltech.com>
2023-07-18 16:53:40 +0500

    check android test report

--- detekt-custom-rules/src/main/java/com/example/detektcustomrules/ObservableValRule.kt
+++ detekt-custom-rules/src/main/java/com/example/detektcustomrules/ObservableValRule.kt
@@ -14,8 +14,22 @@ class ObservableValRule(config: Config): Rule(config) {
         get() = Issue(javaClass.simpleName, Severity.Minor, "This rule is for detecting hardcoded string", Debt.FIVE_MINS)
 
     override fun visitProperty(property: KtProperty) {
-        if (property.text.contains("var", true) && property.text.contains("Observable", true)) {
-            report(CodeSmell(issue,Entity.from(property), "Observable value should not be var. They should be val."))
+        val containsVar = property.text.contains("var", true)
+        val containsObservable = property.text.contains("Observable", true)
+        val containsStateFlow = property.text.contains("StateFlow", true)
+        val containsLiveData = property.text.contains("LiveData", true)
+        if (
+            (containsVar && (containsObservable ||
+                    containsStateFlow ||
+                    containsLiveData))
+        ) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(property),
+                    "Observable, StateFlow or LiveData value should not be var. They should be val."
+                )
+            )
         }
     }
 
